6fcde18da7ba6f66aaf68bc87da186bcf7e44124
Vusal <91078294+vsl-dev@users.noreply.github.com>
2022-06-13 19:22:28 +0400

    vsl-dev

--- server.js
+++ server.js
@@ -123,7 +123,66 @@ app.get('/discord', (req, res) => {
 	res.redirect(config.discord_server)
 })
 
-app.use('/uptime', require("./routes/uptimer.js")) // Uptime system
+// Uptimer
+
+app.post('/uptime/link/add', (req, res) => {
+	if(!req.user) return res.json({"error": "Invalid discord id."})
+	var vsl = Object.values(db.fetch('links'))
+	var lnkhas = vsl.filter(x => x.link == req.body.link)
+    if ((lnkhas == "") == false) {
+	res.redirect('/dashboard')
+	} else {
+	const id = makeid(25)
+	var Data = {
+		link_id: id,
+		link_owner: req.user.id,
+		link: req.body.link,
+		link_when_added: now
+	}
+	db.set(`links.${id}`, Data)
+	res.redirect('/dashboard?success=true')
+}
+})
+
+app.post('/uptime/link/delete/:ID', (req, res) => {
+	try {
+	const owner = db.fetch(`links.${req.params.ID}`).link_owner
+	if((req.user.id == owner) == true) {
+	db.delete(`links.${req.params.ID}`)
+	res.redirect('/dashboard?succes=true')
+	} else {
+	res.redirect('/dashboard?brh')
+	}
+	} catch (err) {
+		res.redirect('/dashboard?success=false&error=true')
+	}
+})
+
+setInterval(() => {
+  var links = db.get("links");
+  if (!links) return;
+  var linkA = Object.values(links);
+  var lnks = linkA.map(c => c.link);
+  lnks.forEach(link => {
+	  const request = require('request');
+request(link, function (error, response, body) {
+	console.log('Pinged - ' + link)
+	})})
+}, 50000);
+
+// Functions
+
+
+function makeid(length) {
+	var result = '';
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() *
+			charactersLength));
+	}
+	return result;
+}
 
 app.listen(8080, () => {
 	console.log("Server running on port - " + port)
